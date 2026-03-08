33190f8c8aa194488dd87848518ee02d5c586904
Marios <mariosranag@gmail.com>
2026-03-08 12:16:59 +0200

    GHOSTTY: WIP Change term bg on ssh (TODO: Add support for other terminals, since it's a fish wrapper).

--- fish/conf.d/ghostty-shell-integration-wrapper.fish
+++ fish/conf.d/ghostty-shell-integration-wrapper.fish
@@ -10,12 +10,36 @@ function install_ssh_wrapper --on-event fish_preexec
         if not functions -q ssh_ghostty
             functions -c ssh ssh_ghostty
         end
-        function ssh --wrap=ssh --description "Wrap ghostty ssh wrapper to change terminal background color when connecting via ssh."
-            printf "\033]11;%s\007" '#000000'
-            ssh_ghostty $argv
-            # Force background reset after session end.
-            background_reset
+    end
+
+    printf AAA
+
+    function ssh --wrap=ssh --description "Wrap ghostty ssh wrapper to change terminal background color when connecting via ssh."
+
+        printf BBB
+        set -l server ""
+        # Parse arguments to find the host (skips flags).
+        for arg in $argv
+            if not string match -q -- "-*" $arg
+                set server $arg
+            end
         end
+        # Remove user@ prefix if present.
+        set server (string replace -r '.*@' '' $server)
+        printf "\033]11;%s\007" (server_bg $server)
+        # Use the ghostty wrapped ssh.
+
+        if functions -q ssh_ghostty
+
+            printf GHOSTTY
+
+            ssh_ghostty $argv
+        else
+            printf ALAC
+            command ssh $argv
+        end
+        # Force background reset after session end.
+        background_reset
     end
 end
 
@@ -24,42 +48,20 @@ function background_reset
     printf "\033]110\007"
     printf "\033]11;%s\007" $DEFAULT_BG
 end
-
-exit
-
 # Function returning background color for a server
 function server_bg
     switch $argv[1]
-        case "server1.domain.tld"
+        case "*.web-bunch.com"
             echo "#422a00"
         case '*'
             echo "#000969"
     end
 end
 
-# Function to change background color using OSC 11
-function background-color
-    printf "\033]11;%s\007" $argv[1]
-end
-# SSH wrapper
-set -l server ""
-
-# Find host argument (skip flags)
-for arg in $argv
-    if not string match -q -- "-*" $arg
-        set server $arg
-    end
-end
-
-# Remove user@ prefix
-set server (string replace -r '.*@' '' $server)
-
-set -l bg (server_bg $server)
+exit
 
 if test -n "$bg"
-    background-color $bg
-    command ssh $argv
-    background-reset
+
 else
     command ssh $argv
     background-reset
