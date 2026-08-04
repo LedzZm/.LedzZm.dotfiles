4a55ad1cc2fe64ac50002528ab3d3c079af94034
Marios <mariosranag@gmail.com>
2026-08-04 21:21:18 +0300

    HYPR: Starts rewrite of hyprland config to lua in preparation for 0.56.

---
+++ hypr/hyprland.lua
@@ -0,0 +1,38 @@
+MainMod = "SUPER"
+Terminal = "ghostty"
+FileManager = "thunar"
+Browser = "vivaldi"
+Menu = "wofi --show drun"
+
+-- Base Config
+require(os.getenv("XDG_CONFIG_HOME") .. "/colors/colors")
+require("config/hypr-core-legacy")
+require("config/autostart")
+-- Eye Candy
+require("config/decorations")
+
+-- 	#source = ~/.config/hypr/config/animations
+--
+-- Input
+--
+-- #source = ~/.config/hypr/config/mouse
+require("config/keybinds")
+-- 	#source = ~/.config/hypr/config/keyboard
+-- 	#source = ~/.config/hypr/config/keybinds
+--
+-- # Window and layer management.
+--
+-- #source = ~/.config/hypr/config/windowrules
+--
+-- # Workspaces (Keybinds & rules)
+--
+-- 	#source = ~/.config/hypr/config/workspaces
+--
+-- # App Configuration
+-- 	#source = ~/.config/hypr/config/apps/*
+--
+-- # Config / Feature that are either unstable, OR I am not sure I want / need them.
+-- 	#source = ~/.config/hypr/experimental
+--
+-- # User / Machine specific Configuration
+-- --	#source = ~/.config/hypr/config/user-config --
