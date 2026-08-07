c95549d41e40652d0532724eb1fea466402388ee
Marios <mariosranag@gmail.com>
2026-08-07 21:15:41 +0300

    HYPR: refactors workspaces and keyboard config to lua.

---
+++ hypr/config/workspaces.lua
@@ -0,0 +1,26 @@
+for key = 1, 9 do
+	-- Switch workspaces with MainMod + [1-9]
+	hl.bind(MainMod .. " + " .. key, hl.dsp.focus({ workspace = key }))
+	-- Move active window to a workspace and follow
+	hl.bind(MainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = key }))
+	-- Move active window to a workspace without swiching focus
+	hl.bind(MainMod .. " + CONTROL + " .. key, hl.dsp.window.move({ workspace = key, follow = false }))
+end
+
+-- Cycle through workspaces
+hl.bind(MainMod .. " + right", hl.dsp.focus({ workspace = "+1" }))
+hl.bind(MainMod .. " + mouse_down", hl.dsp.focus({ workspace = "+1" }))
+hl.bind(MainMod .. " + left", hl.dsp.focus({ workspace = "-1" }))
+hl.bind(MainMod .. " + mouse_up", hl.dsp.focus({ workspace = "-1" }))
+
+-- Special workspaces
+local specialWorkspaces = {
+	["C"] = "console",
+	["O"] = "obsidian",
+}
+for key, workspaceName in pairs(specialWorkspaces) do
+	-- Bind toggle and move to workspace
+	hl.bind(MainMod .. " + " .. key, hl.dsp.workspace.toggle_special(workspaceName))
+	hl.bind(MainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = "special:" .. workspaceName }))
+	hl.bind(MainMod .. " + CONTROL + " .. key, hl.dsp.window.move({ workspace = workspaceName, follow = false }))
+end
--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -16,17 +16,15 @@ require("config/decorations")
 --
 -- #source = ~/.config/hypr/config/mouse
 require("config/keybinds")
--- 	#source = ~/.config/hypr/config/keyboard
--- 	#source = ~/.config/hypr/config/keybinds
+require("config/keyboard")
 --
 -- # Window and layer management.
 --
 -- #source = ~/.config/hypr/config/windowrules
 --
--- # Workspaces (Keybinds & rules)
---
--- 	#source = ~/.config/hypr/config/workspaces
---
+-- Workspaces (Keybinds & rules)
+require("config/workspaces")
+
 -- App Configuration
 require(os.getenv("XDG_CONFIG_HOME") .. "/hypr/config/apps/*.lua")
 -- 	#source = ~/.config/hypr/config/apps/*
