fed2c8777d269d2680a807277a982bbdd2f9cb8c
Temporary vm <mariosranag@gmail.com>
2026-08-23 21:25:32 +0300

    HYPR: Improve file structure for hyprland config.

---
+++ hypr/config/init.lua
@@ -0,0 +1,21 @@
+-- Base Config
+require("config.hypr-core-legacy")
+require("config.autostart")
+
+-- Eye Candy
+require("config.decorations")
+require("config.animations")
+
+-- Input
+require("config.mouse")
+require("config.keybinds")
+require("config.keyboard")
+
+-- Window and layer management.
+require("config.windowrules")
+
+-- Workspaces (Keybinds & rules)
+require("config.workspaces")
+
+-- App Configuration
+require("config.apps")
--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -6,29 +6,8 @@ Menu = "wofi --show drun"
 
 -- Helpful tools
 require("utils")
-
--- Base Config
-require("config.hypr-core-legacy")
-require("config.autostart")
-
--- Eye Candy
-require("config.decorations")
-require("config.animations")
-
--- Input
-require("config.mouse")
-require("config.keybinds")
-require("config.keyboard")
-
--- Window and layer management.
-require("config.windowrules")
-
--- Workspaces (Keybinds & rules)
-require("config.workspaces")
-
--- App Configuration
-require("config.apps")
-
+-- Base Hyprland config
+require("config")
 -- Gaming
 -- TODO: decide if this should be optional
 -- require("config.gaming")
@@ -37,7 +16,7 @@ require("config.apps")
 -- 	#source = ~/.config.hypr/experimental
 --
 -- User / Machine specific Configuration
-pcall(require, "config.user-config")
+pcall(require, "user-config")
 
 -- General Settings
 hl.config({
